a3fc1e4d7af914f7e6049b864153c68b2de81301
gbhobbytech <gbhobbytech@gmail.com>
2026-05-01 12:49:04 +1000

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-    basic.showString("Test")
-})
+//% color="#005A9C" icon="\uf26c" block="OLED"
+namespace oled {
+    //% block="OLED test"
+    export function test(): void {
+        basic.showString("OLED")
+    }
+}
